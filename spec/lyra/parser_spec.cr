816a8f0e856be55c3904d2e45ec0cb3d2bf11e67
Stephanie Wilde-Hobbs <steph@rx14.co.uk>
2020-06-23 18:17:51 +0100

    Add Lyra parser

---
+++ spec/lyra/parser_spec.cr
@@ -0,0 +1,202 @@
+require "../spec_helper"
+
+private def new_parser(str : String)
+  io = IO::Memory.new
+  io << str
+  Lyra::Parser.new(io.rewind)
+end
+
+describe Lyra::Parser do
+  it "parses" do
+    parser = new_parser <<-'EOF'
+      FOO bar baz :arg with spaces
+      BAZ :bar
+
+      EOF
+
+    parser.read_name.should eq("FOO")
+    parser.command_size.should eq(4)
+    parser.read_arg.should eq("bar")
+    parser.command_size.should eq(8)
+    parser.read_arg.should eq("baz")
+    parser.command_size.should eq(12)
+    parser.read_arg.should eq("arg with spaces")
+    parser.command_size.should eq(29)
+    parser.read_name.should eq("BAZ")
+    parser.command_size.should eq(4)
+    parser.read_arg.should eq("bar")
+    parser.command_size.should eq(9)
+    parser.read_name.should be_nil
+  end
+
+  describe "#read_name" do
+    it "doesn't read command-name when positioned at arg" do
+      parser = new_parser <<-'EOF'
+        CMD arg :arg
+        CMD arg :arg
+
+        EOF
+
+      parser.read_name.should eq("CMD")
+      expect_raises(Lyra::Parser::ParseException, "can't read command-name when positioned at command-arg") do
+        parser.read_name
+      end
+      parser.read_arg.should eq("arg")
+      expect_raises(Lyra::Parser::ParseException, "can't read command-name when positioned at command-arg") do
+        parser.read_name
+      end
+      parser.read_arg.should eq("arg")
+      parser.read_name.should eq("CMD")
+      expect_raises(Lyra::Parser::ParseException, "can't read command-name when positioned at command-arg") do
+        parser.read_name
+      end
+      parser.read_arg.should eq("arg")
+      expect_raises(Lyra::Parser::ParseException, "can't read command-name when positioned at command-arg") do
+        parser.read_name
+      end
+      parser.read_arg.should eq("arg")
+      parser.read_name.should be_nil
+    end
+
+    it "doesn't parse too-long command-name" do
+      parser = new_parser <<-EOF
+        AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA :bar
+        AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA :bar
+        BAZ :bar
+
+        EOF
+
+      parser.read_name.should eq("A" * 32)
+      parser.read_arg.should eq("bar")
+
+      expect_raises(Lyra::Parser::ParseException, "command-name is longer than 32 characters") do
+        parser.read_name.should eq("A" * 33)
+      end
+    end
+
+    it "doesn't parse non-alphanumeric command-name" do
+      parser = new_parser <<-EOF
+        ABCDEFGHIJKLMNOPQRSTUVWXYZ :
+        1234567890 :
+        lowercase :
+
+        EOF
+
+      parser.read_name.should eq("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+      parser.read_arg.should eq("")
+
+      parser.read_name.should eq("1234567890")
+      parser.read_arg.should eq("")
+
+      expect_raises(Lyra::Parser::ParseException, %(command-name "lowercase" does not contain only A-Z0-9)) do
+        parser.read_name.should eq("")
+      end
+    end
+  end
+
+  describe "#read_arg" do
+    it "reads IO" do
+      parser = new_parser <<-'EOF'
+        FOO :bar
+        BAZ :bar
+
+        EOF
+
+      parser.read_name.should eq("FOO")
+      parser.read_arg do |io|
+        io.read_char.should eq('b')
+        io.read_char.should eq('a')
+        io.read_char.should eq('r')
+        io.read_char.should eq(nil)
+      end
+      parser.read_name.should eq("BAZ")
+      parser.read_arg do |io|
+        io.read_char.should eq('b')
+        io.read_char.should eq('a')
+        io.read_char.should eq('r')
+        io.read_char.should eq(nil)
+      end
+      parser.read_name.should be_nil
+    end
+
+    it "doesn't read command-arg when positioned at name" do
+      parser = new_parser <<-'EOF'
+        CMD arg :arg
+        CMD arg :arg
+
+        EOF
+
+      parser.read_name.should eq("CMD")
+      expect_raises(Lyra::Parser::ParseException, "can't read command-name when positioned at command-arg") do
+        parser.read_name
+      end
+      parser.read_arg.should eq("arg")
+      expect_raises(Lyra::Parser::ParseException, "can't read command-name when positioned at command-arg") do
+        parser.read_name
+      end
+      parser.read_arg.should eq("arg")
+      parser.read_name.should eq("CMD")
+      expect_raises(Lyra::Parser::ParseException, "can't read command-name when positioned at command-arg") do
+        parser.read_name
+      end
+      parser.read_arg.should eq("arg")
+      expect_raises(Lyra::Parser::ParseException, "can't read command-name when positioned at command-arg") do
+        parser.read_name
+      end
+      parser.read_arg.should eq("arg")
+      parser.read_name.should be_nil
+    end
+
+    it "raises on unexpected EOF" do
+      {"CMD", "CMD ", "CMD \n", "CMD :"}.each do |test|
+        parser = new_parser "CMD "
+        parser.read_name.should eq("CMD")
+        expect_raises(IO::EOFError) do
+          parser.read_arg
+        end
+      end
+    end
+
+    it "raises on too-large command (non-end)" do
+      str = "CMD arg1 arg2 #{"a" * (65535 - 15)} "
+      str.bytesize.should eq(65535)
+      parser = new_parser(str)
+      parser.read_name.should eq("CMD")
+      parser.read_arg.should eq("arg1")
+      parser.read_arg.should eq("arg2")
+      parser.command_size.should eq(14)
+      parser.read_arg.should eq("a" * (65535 - 15))
+
+      str = "CMD arg1 arg2 #{"a" * (65535 - 14)} "
+      str.bytesize.should eq(65536)
+      parser = new_parser(str)
+      parser.read_name.should eq("CMD")
+      parser.read_arg.should eq("arg1")
+      parser.read_arg.should eq("arg2")
+      expect_raises(Lyra::Parser::ParseException, "command size was greater than 65535 bytes") do
+        parser.read_arg
+      end
+    end
+
+    it "raises on too-large command (end)" do
+      str = "CMD arg1 arg2 :#{"a" * (65535 - 16)}\n"
+      str.bytesize.should eq(65535)
+      parser = new_parser(str)
+      parser.read_name.should eq("CMD")
+      parser.read_arg.should eq("arg1")
+      parser.read_arg.should eq("arg2")
+      parser.command_size.should eq(14)
+      parser.read_arg.should eq("a" * (65535 - 16))
+
+      str = "CMD arg1 arg2 :#{"a" * (65535 - 15)}\n"
+      str.bytesize.should eq(65536)
+      parser = new_parser(str)
+      parser.read_name.should eq("CMD")
+      parser.read_arg.should eq("arg1")
+      parser.read_arg.should eq("arg2")
+      expect_raises(Lyra::Parser::ParseException, "command size was greater than 65535 bytes") do
+        parser.read_arg
+      end
+    end
+  end
+end
---
+++ src/lyra/parser.cr
@@ -0,0 +1,100 @@
+class Lyra::Parser
+  enum State
+    CommandStart
+    ArgStart
+  end
+
+  class ParseException < Exception
+  end
+
+  getter command_size : Int32 = 0
+
+  def initialize(@io : IO)
+    @state = State::CommandStart
+  end
+
+  COMMAND_SIZE_MAX = 65_535
+
+  # Returns the name of the parsed command, or nil if the connection was closed.
+  def read_name : String?
+    error "can't read command-name when positioned at command-arg" unless @state.command_start?
+
+    name = @io.gets(' ', limit: 32 + 1, chomp: true)
+    return nil unless name
+
+    error "command-name is longer than 32 characters" if name.size > 32
+    unless name.each_char.all? { |c| command_name_char? c }
+      error "command-name #{name.inspect} does not contain only A-Z0-9"
+    end
+
+    @command_size = name.bytesize + 1
+    @state = State::ArgStart
+
+    name
+  end
+
+  private def command_name_char?(char)
+    'A' <= char <= 'Z' || '0' <= char <= '9'
+  end
+
+  # Returns true if another argument can be read from this command.
+  def has_arg?
+    @state.arg_start?
+  end
+
+  # Reads an argument of the currently parsing command, as a string.
+  def read_arg : String
+    read_arg do |arg|
+      String.build do |str|
+        IO.copy(arg, str)
+      end
+    end
+  end
+
+  # Reads an argument of the currently parsing command, yielding the argument
+  # data in an IO passed to the provided block.
+  def read_arg(& : IO -> _)
+    error "can't read command-arg when positioned at command-name" unless @state.arg_start?
+
+    # TODO: optimize
+
+    io = IO::Memory.new
+
+    byte = @io.read_byte
+    raise IO::EOFError.new unless byte
+    @command_size += 1
+
+    if byte == ':'.ord
+      last_arg = true
+      target_byte = '\n'.ord.to_u8
+    else
+      io.write_byte(byte)
+
+      last_arg = false
+      target_byte = ' '.ord.to_u8
+    end
+
+    while @command_size < COMMAND_SIZE_MAX
+      byte = @io.read_byte
+      raise IO::EOFError.new unless byte
+      @command_size += 1
+
+      break if byte == target_byte
+
+      io.write_byte(byte)
+    end
+
+    error "command size was greater than #{COMMAND_SIZE_MAX} bytes" unless byte == target_byte
+
+    if last_arg
+      # We read the newline so it's the start of a new command
+      @state = State::CommandStart
+    end
+
+    yield io.rewind
+  end
+
+  private def error(string)
+    raise ParseException.new(string)
+  end
+end
